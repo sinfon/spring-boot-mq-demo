aaea67e2e94f677e2dd06bfdd443e0c0be35bff4
singoasher <singoasher@163.com>
2018-06-22 14:56:50 +0800

    init

---
+++ src/main/java/top/sinfonia/demo/mq/SpringBootMqDemoApplication.java
@@ -0,0 +1,12 @@
+package top.sinfonia.demo.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMqDemoApplication.class, args);
+	}
+}
